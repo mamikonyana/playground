fb5df37d13bf82cf1692402e1dd0a6b54d2edb86
Arsen Mamikonyan <arsen@mamikonyan.am>
2016-11-29 00:05:53 -0500

    add duration estimate

--- 1_dollar_roulette/play.py
+++ 1_dollar_roulette/play.py
@@ -4,6 +4,7 @@ Report ratio of win scenarios when playing one dollar at a time.
 Win = reach stop-money amount
 
 for default values ratio ~= 0.1084
+for default values expected time ~= 297.6
 """
 from __future__ import print_function
 import argparse
@@ -23,15 +24,19 @@ def parse_args(*argument_array):
 def play_roulette(init_money, end_money):
   money = init_money
   wining_odds = 18. / 38
-  while money not in (0, end_money):
+  for i in range(1, 10000000):
     money += 1 if random.random() < wining_odds else -1
-  return money == end_money
+    if money in (0, end_money):
+      return money == end_money, i
+  return None, None
 
 
 def main(args):
-  results = [1. * play_roulette(args.start_money, args.stop_money)
+  results = [play_roulette(args.start_money, args.stop_money)
              for i in range(args.num_trials)]
-  print(np.mean(results))
+  outcomes, lengths = zip(*results)
+  print('Exptected ratio of wins', np.mean(outcomes))
+  print('Expected play turns', np.mean(lengths))
 
 
 if __name__ == '__main__':
